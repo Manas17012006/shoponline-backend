67ac350ba0ef47dca8f9dd59f15b314d2f1fb376
Manas17012006 <mkawathalkar@gmail.com>
2026-01-03 03:45:36 +0530

    idk

--- middleware/auth.mw.js
+++ middleware/auth.mw.js
@@ -1,19 +1,15 @@
 const userModel = require('../models/userModel.model');
-const jwt = require('jsonwebtoken');
 
 const userauth = async (req, res, next) => {
   try {
-    // Get token from Authorization header
-    const token = req.headers.authorization?.split(" ")[1];
+    // Get token either from header or req.body (your choice)
+    const token = req.headers.authorization?.split(" ")[1] || req.body.token;
     if (!token) {
       return res.status(401).json({ success: false, message: "Please login to get started!" });
     }
 
-    // Decode token (if using JWT)
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
     // Find user by token stored in DB
-    const user = await userModel.findOne({ _id: decoded.id, token: token });
+    const user = await userModel.findOne({ token: token });
     if (!user) {
       return res.status(401).json({ success: false, message: "Invalid token" });
     }
@@ -22,7 +18,7 @@ const userauth = async (req, res, next) => {
     req.userId = user._id;
     next();
   } catch (err) {
-    return res.status(401).json({ success: false, message: "Invalid or expired token" });
+    return res.status(401).json({ success: false, message: "Invalid token" });
   }
 };
 
